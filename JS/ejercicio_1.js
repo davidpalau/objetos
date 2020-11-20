4e26d49b9f3d0a581d28ec94444f9bb3d81f3e21
davidpalau <palau.david@hotmail.com>
2020-11-20 22:00:19 +0100

    Update ejercicio_1.js

--- JS/ejercicio_1.js
+++ JS/ejercicio_1.js
@@ -1,10 +1,16 @@
-let y = prompt('Introduce el valor de Y');
-let x = prompt('Introduce el valor de X');
+//let y = prompt('Introduce el valor de Y');
+//let x = prompt('Introduce el valor de X');
+let nuevoPunto = new PuntosCardinales(5, 3);
+
+console.log(nuevoPunto.mostrar());
 
 function PuntosCardinales(y, x) {
   this.eje_y = isNaN(y) ? 0 : y;
   this.eje_x = isNaN(x) ? 0 : x;
-
+return 'Cordenada Y =', this.eje_y, ' cordenada X=', eje_x;
+  function mostrar() {
+    
+  }
   let cambiar = (y, x) => {
     this.eje_y = y;
     this.eje_x = x;
