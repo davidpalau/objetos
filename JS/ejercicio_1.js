64c2a1bfc9bb0ea3dca101fee22fe20d57df520c
davidpalau <palau.david@hotmail.com>
2020-11-20 21:38:22 +0100

    Update ejercicio_1.js

--- JS/ejercicio_1.js
+++ JS/ejercicio_1.js
@@ -1,4 +1,19 @@
-var PuntosCardinales = {
-  eje_y: 0.0,
-  eje_x: 0.0,
-};
+let y = prompt('Introduce el valor de Y');
+let x = prompt('Introduce el valor de X');
+
+function PuntosCardinales(y, x) {
+  this.eje_y = isNaN(y) ? 0 : y;
+  this.eje_x = isNaN(x) ? 0 : x;
+
+  let cambiar = (y, x) => {
+    this.eje_y = y;
+    this.eje_x = x;
+  };
+
+  let copiaPunto = () => new PuntosCardinales(this.eje_y, this.eje_x);
+
+  let Sumar = (y, x) => {
+    this.eje_y = this.eje_y + y;
+    this.eje_x = this.eje_x + x;
+  };
+}
