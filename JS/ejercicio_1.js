6df7f4661d720307a89ac68e4d50caeebfcfc8e9
David Palau Morant <palau.david@hotmail.com>
2020-11-21 12:12:52 +0100

    Terminado el primer ejercicio

--- JS/ejercicio_1.js
+++ JS/ejercicio_1.js
@@ -1,25 +1,22 @@
-//let y = prompt('Introduce el valor de Y');
-//let x = prompt('Introduce el valor de X');
-let nuevoPunto = new PuntosCardinales(5, 3);
+function PuntosCardinales(y, x) { //Prototipo un objeto punto.
+    this.eje_y = isNaN(y) ? 0 : y;
+    this.eje_x = isNaN(x) ? 0 : x;
+}
 
-console.log(nuevoPunto.mostrar());
-
-function PuntosCardinales(y, x) {
-  this.eje_y = isNaN(y) ? 0 : y;
-  this.eje_x = isNaN(x) ? 0 : x;
-return 'Cordenada Y =', this.eje_y, ' cordenada X=', eje_x;
-  function mostrar() {
-    
-  }
-  let cambiar = (y, x) => {
+PuntosCardinales.prototype.cambiar = function(y, x) {
     this.eje_y = y;
     this.eje_x = x;
-  };
-
-  let copiaPunto = () => new PuntosCardinales(this.eje_y, this.eje_x);
-
-  let Sumar = (y, x) => {
+};
+PuntosCardinales.prototype.sumar = function(x, y) {
     this.eje_y = this.eje_y + y;
     this.eje_x = this.eje_x + x;
-  };
 }
+
+PuntosCardinale
+let nuevoPunto = new PuntosCardinales(5, 3);
+
+document.write("<h1>Ejercicio 1 de objetos</h1>");
+document.write("<p> Este punto tiene las cordedas:(<strong> Y = " + nuevoPunto.eje_y + ", X = " + nuevoPunto.eje_x + "</strong>)</p>");
+nuevoPunto.cambiar(7, 9);
+
+document.write("<p> Este puntotiene nuevas cordendas son:(<strong> Y = " + nuevoPunto.eje_y + ", X = " + nuevoPunto.eje_x + "</strong>)</p>");
